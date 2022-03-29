d4bee15d9b34c99311afeae9fc56433092bfadeb
mazurenka <an.drej@mail.ru>
2022-03-29 22:46:33 +0200

    2

--- src/state.tsx
+++ src/state.tsx
@@ -3,19 +3,68 @@ import React from "react";
 let a = 10
 a = 12
 
+// types
+//
+// interface IMan {
+//     name: string
+//     age: number
+// }
+//
+// type CarType = {
+//     model: string
+//     year: number
+// }
+//
+// let man: IMan = {name: 'Dim', age: 21}
+// let man1: IMan = {name: 'Andre', age: 31}
+// let car: CarType = {model: 'Tesla', year: 2022}
+//
+// //
 
-interface IMan {
+type ManType = {
     name: string
     age: number
 }
 
-type CarType = {
-    model: string
-    year: number
-}
+let b: ManType[] = [{name: 'Andre', age: 22}, {name: 'Victor', age: 21}]
 
-let man: IMan = {name: 'Dim', age: 21}
-let man1: IMan = {name: 'Andre', age: 31}
-let car: CarType = {model: 'Tesla', year: 2022}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
