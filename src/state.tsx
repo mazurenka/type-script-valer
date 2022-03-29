b979a0962c08803698bd3f901512bfb07676d8f9
mazurenka <an.drej@mail.ru>
2022-03-29 22:31:45 +0200

    1

---
+++ src/state.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+let a = 10
+a = 12
+
+
+interface IMan {
+    name: string
+    age: number
+}
+
+type CarType = {
+    model: string
+    year: number
+}
+
+let man: IMan = {name: 'Dim', age: 21}
+let man1: IMan = {name: 'Andre', age: 31}
+let car: CarType = {model: 'Tesla', year: 2022}
+
+
